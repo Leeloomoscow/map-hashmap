3e32d6e8190d4ea7e26f664cb6e5bbf6adcb46d6
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-11-17 18:05:42 +0300

    HashMap

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        Map<Integer, String> persons = new HashMap<>();
+        persons.put(465647478, "Ivanov Ivan1 Ivanovich");
+        persons.put(465647479, "Ivanov Ivan2 Ivanovich");
+        persons.put(465647490, "Ivanov Ivan3 Ivanovich");
+        persons.put(465647495, "Ivanov Ivan4 Ivanovich");
+
+        //перебор Коллекции
+        for (Map.Entry<Integer, String> item : persons.entrySet()) {
+            System.out.println("Ключ: " + item.getKey() + " Значение: " + item.getValue());
+        }
+//        System.out.println(persons.get(465647490));
+//        System.out.println(persons.keySet()); //Список всех ключей
+//        System.out.println(persons.values()); //Список всех значений
+//        System.out.println(persons.replace(465647490, "Замена")); //Замена key на новое значение
+//        System.out.println(persons.get(465647490));
+//        System.out.println(persons.remove(465647490)); //Удаление ненужного ключа
+//        System.out.println(persons.values());
+//        System.out.println(persons.size()); //Посмотреть количество элементов в коллекции
+//        persons.clear(); //Удаление всех элементов в коллекции. У данного метода нет вывода на консоль
+
     }
 }
 
